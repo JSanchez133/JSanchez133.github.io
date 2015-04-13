221fd013df73e51c7e67c9f0f9d537f637fea4f8
Joel Sanchez <JSanchez@kippnyccp.org>
2015-04-13 13:20:35 -0400

    Update javascript.js

--- ShoppingList/javascript.js
+++ ShoppingList/javascript.js
@@ -1,11 +1,6 @@
 $(document).ready(function(){
-  $('#add').on('click',function(){
-    var item=$('input').val();
-    $("div").append('<p>'+item+'</p>')
-    });
-    $('#remove').on('click',function(){
-      var item=$('input').val()
-      $('div').empty();
-    })
-    }
-)
+   $('button').on('click',function(){
+    var input=$('input').val();
+    $('#mydiv').append("<p>"+input+"</p>");
+   });
+});
