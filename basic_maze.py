d2a5641b19c4ae108eae9ffa44ad8fe52769dee6
Joel Sanchez <JSanchez@kippnyccp.org>
2015-05-20 13:09:59 -0400

    Update basic_maze.py

--- basic_maze.py
+++ basic_maze.py
@@ -1,1 +1,19 @@
+from gopigo import 
+import time
+import math
+
+set_speed(100)
+
+enc_tgt(1,1,18)
+time.sleep(.1)
+fwd()
+time.speed(3)
+
+def move_forward(feet):
+
+def turn_right():
+enc_tgt(1,0,18)
+time.sleep(.1)
+right()
+time.sleep(2)
 
