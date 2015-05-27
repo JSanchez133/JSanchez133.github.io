ae482eb6a97f32a1b94a35e9536a748c943f3ff4
Joel Sanchez <JSanchez@kippnyccp.org>
2015-05-27 13:50:19 -0400

    Update sensor.py

--- sensor.py
+++ sensor.py
@@ -1,4 +1,11 @@
 from gopigo import *
 
-while us_dist(15) > 20:
-  fwd()
+def turn_right:
+  enc_tgt(1,0,15)
+  right()
+
+while 1>0:
+  while us_dist(15) > 20:
+    fwd()
+  stop()
+  turn_right()
